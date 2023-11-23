4ca3829f96e7ea84e9bdf527215c31692c29916e
Rastele <unrastele@gmail.com>
2023-11-23 17:04:07 +0100

    Created User class

---
+++ src/models/User.php
@@ -0,0 +1,46 @@
+<?php
+class User
+{
+    private $email;
+    private $username;
+    private $password;
+
+    public function __construct(string $email, string $username, string $password)
+    {
+        $this->email = $email;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
+}
