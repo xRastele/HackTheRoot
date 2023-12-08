9aa837ac89c5d2a8cee4eb076d449a0e36bc410c
Rastele <unrastele@gmail.com>
2023-12-08 23:39:58 +0100

    Adjusted to display feedback information during login

---
+++ public/views/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/login.css">
+    <title>HackTheRoot $ Login</title>
+</head>
+<body>
+<div class="container">
+    <div class="logo">
+        <img src="public/img/logo.svg" alt="Logo">
+    </div>
+    <div class="login-container">
+        <form class="login" action="login" method="POST">
+            <div class="messages">
+                <?php if(isset($messages)) {
+                    foreach ($messages as $message) {
+                        echo $message;
+                    }
+                }
+                ?>
+            </div>
+            <input name="email" type="text" placeholder="Email">
+            <input name="password" type="password" placeholder="Password">
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</div>
+</body>
