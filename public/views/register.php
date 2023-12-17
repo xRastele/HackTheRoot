a84dac9214cf720e0967cbdbfa0510513d051c30
Rastele <unrastele@gmail.com>
2023-12-17 12:44:25 +0100

    Changed layout for better look

--- public/views/register.php
+++ public/views/register.php
@@ -2,6 +2,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/register.css">
     <title>HackTheRoot $ Register</title>
 </head>
 <body>
@@ -15,6 +16,7 @@
             <input name="username" type="username" placeholder="Username">
             <input name="password" type="password" placeholder="Password">
             <button>Sign up</button>
+            <p>Already have an account? Login <a href="/login">here</a>.</p>
         </form>
     </div>
 </div>
